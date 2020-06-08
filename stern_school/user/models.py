e28387217e333f56f8986f1b5e46782bc08e2806
anton-serozhechkin <serezhechkintosh@gmail.com>
2020-06-08 20:57:01 +0300

    change model user, add login view

--- stern_school/user/models.py
+++ stern_school/user/models.py
@@ -13,17 +13,21 @@ class ClassNumber(models.Model):
 
 
 class User(AbstractUser):
+    username = None
+    email = models.EmailField(verbose_name='email address', unique=True)
     middle_name = models.CharField('Отчество', max_length=50)
     telephone_number = models.CharField('Телефонный номер', max_length=12)
     class_number = models.ForeignKey(ClassNumber, on_delete=models.CASCADE, verbose_name='Номер класса')
     name_school = models.CharField('Название школы', max_length=50)
     date_birthday = models.DateField(verbose_name='День рождения')
-    telephone_number = models.CharField('Телефонный номер', max_length=12)
     is_parent = models.BooleanField('Родитель', default=False, blank=True, null=True)
     children_telephone_number = models.CharField('Телефонный номер', max_length=12, blank=True, null=True)
     short_info = models.CharField('Краткая информация', max_length=100, blank=True, null=True)
     full_info = models.CharField('Полная информация', max_length=2000, blank=True, null=True)
     image = models.ImageField('Фото преподавателя', upload_to='pictures/teachers/', blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = 'Пользователь'
--- stern_school/user/views.py
+++ stern_school/user/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('payment')
+    else:
+        return render(request, 'signin.html')
+    return render(request, 'signin.html')
+
 def not_found_view(request, exception):
     return render(request, 'errors/404.html')
     
